fba1069ce514c8558ff8889b991c54b7384c7a2d
Stuart Jansen <sjansen@buscaluz.org>
2022-10-15 11:19:31 -0400

    add build info to version command

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -5,10 +5,10 @@ import (
 )
 
 var cli struct {
-	Fmt      fmtCmd      `kong:"cmd,help='Reformat files in the standard style'"`
-	Help     helpCmd     `kong:"cmd,help='Show this help text'"`
-	Generate generateCmd `kong:"cmd,help='Generate Go code for the schema directory'"`
-	Version  versionCmd  `kong:"cmd,help='Show the current Shiny version'"`
+	Fmt      fmtCmd      `kong:"cmd,help='Reformat files in the standard style.'"`
+	Help     helpCmd     `kong:"cmd,help='Show this help text.'"`
+	Generate generateCmd `kong:"cmd,help='Generate Go code from the schema directory.'"`
+	Version  versionCmd  `kong:"cmd,help='Show build version.'"`
 }
 
 // ParseAndRun parses command line arguments then runs the matching command.
--- internal/cli/version.go
+++ internal/cli/version.go
@@ -1,12 +1,30 @@
 package cli
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
 
 type versionCmd struct {
 	version string
+	Verbose bool `kong:"short='v',help='Show build details.'"`
 }
 
 func (cmd *versionCmd) Run() error {
-	fmt.Println(cmd.version)
+	if cmd.Verbose {
+		fmt.Println("Shiny", cmd.version)
+		fmt.Println()
+		fmt.Println("Build Info:")
+		fmt.Println("===========")
+		info, err := debug.ReadBuildInfo()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		} else {
+			fmt.Println(info)
+		}
+	} else {
+		fmt.Println(cmd.version)
+	}
 	return nil
 }
